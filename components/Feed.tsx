9e8a273e7e77fb84b3c2a190bb54adab2d658dd6
EzmOdeus <shafrax500@gmail.com>
2024-05-29 00:35:11 +0300

    V1

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,4 +1,3 @@
-"use client"
 import {
     collection,
     getDocs,
@@ -9,15 +8,12 @@ import {
 
 import { app } from '../app/firebase';
 import Post from './Post';
-import { useEffect } from 'react';
 
 export default async function Feed() {
     const db = getFirestore(app);
     const q = query(collection(db, 'posts'), orderBy('timestamp', 'desc'));
     const querySnapshot = await getDocs(q);
-    console.log("🚀 ~ Feed ~ querySnapshot:", querySnapshot)
- 
-    const data:any = [];
+    let data:any = [];
     querySnapshot.forEach((doc) => {
         data.push({ id: doc.id, ...doc.data() });
     });
